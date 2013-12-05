84bd352c544bc03173c046c113ff40a30369a6e9
Marco Agner <marco.agner@live.com>
2013-12-05 18:58:27 -0200

    Add js

--- libs/version-map.js
+++ libs/version-map.js
@@ -11,7 +11,7 @@
   semver = require('semver');
 
   VersionMap = (function() {
-    VersionMap.prototype.version = '0.6.0';
+    VersionMap.prototype.version = '0.7.1';
 
     function VersionMap(options) {
       this.updateVersion = __bind(this.updateVersion, this);
@@ -180,7 +180,8 @@
         updatedRegistryIndexJSON = _this.updateRegistryIndexJSON(registryIndexBuffer, packageJSON, tag);
         return _this.uploadRegistryIndex(updatedRegistryIndexJSON);
       }).fail(function(err) {
-        return console.err("Could not update registry index!", err);
+        console.err("Could not update registry index!", err);
+        return err;
       });
     };
 
